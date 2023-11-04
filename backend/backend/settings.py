61ea2b45356694794bed7693c8d5dfaf0cc83149
chy <chenhaoy21@mails.tsinghua.edu.cn>
2023-11-04 21:57:14 +0800

    连接到数据库

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fancypet',
+        'USER': '214a',
+        'PASSWORD': 'fancy',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
