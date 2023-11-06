ca32417bab96f221c93eccab2364619a519e152a
VampireDetached <1478173643@qq.com>
2023-11-06 20:40:15 +0900

    login-v3

--- frontend/miniprogram/pages/index/index.ts
+++ frontend/miniprogram/pages/index/index.ts
@@ -7,13 +7,13 @@ Page({
     motto: 'Hello World',
     userInfo: {
         nickName : "cuber",
-        avatarUrl : "",
+        avatarUrl : "/test.jpg",
         gender : "0",
         province : "hainan",
         city : "haikou",
         country : "China"
     },
-    hasUserInfo: false,
+    hasUserInfo: true,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
     canIUseOpenData: false, // 如需尝试获取用户信息可改为false
@@ -42,7 +42,8 @@ Page({
     
         that.setData({   
             nickName : "cuber",
-            avatarUrl:userInfo.avatarUrl,
+            // avatarUrl:userInfo.avatarUrl,
+            avatarUrl:"/test.jpg",
             gender: "0",
             province: "hainan",
             city: "haikou",
@@ -53,27 +54,76 @@ Page({
   },
   getUserProfile() {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-            nickName : res.userInfo.nickName,
-            avatarUrl:res.userInfo.avatarUrl,
-            gender: res.userInfo.gender,
-            province: res.userInfo.province,
-            city: res.userInfo.city,
-            country: res.userInfo.country,
-            hasUserInfo: true,
-            canIUseOpenData: true
-        })
-        wx.setStorage({
-            data: res.userInfo,
-            key: 'userInfo',
-        })
+    wx.getSetting({
+        success: (res) => {
+          console.log(res)
+   
+          // 判断用户信息是否有授权
+          if (!res.authSetting['scope.userInfo']) {
+            // 获取授权
+            wx.authorize({
+              scope: 'scope.userInfo',
+              success: (e) => {
+                console.log('authorize: ', e)
+   
+                // 调用你们后端写的登录接口
+                this.login()
+              },
+              fail: (err) => {
+                console.log('authorize-err: ', err)
+              }
+            })
+          } else {
+            console.log('已授权')
+            // 调用你们后端写的登录接口
+            this.login()
+          }
+        },
+        fail: (err)=> {
+          console.log(err)
+        }
+      })
+  },
+  login() {
+ 
+    let _this = this;
+ 
+    // 获取微信登录的code
+    wx.login({
+      success (e) {
+        if (e.code) {
+            wx.showToast({
+                title: '登录成功！' + e.errMsg,
+                icon: 'none',
+                duration: 2000
+              })
+          // 请求后端写的登录逻辑接口
+          // ......
+            wx.request({
+                url:'/login',
+                data:{
+                    code:e.code
+                },
+                method:"POST",
+                header: {'content-type': 'application/json' //
+                },
+                success(res) {
+                    console.log(res.data)
+                    
+                }
+            })
+ 
+        } else {
+          wx.showToast({
+            title: '登录失败！' + e.errMsg,
+            icon: 'none',
+            duration: 2000
+          })
+          console.log('登录失败！' + e.errMsg)
+        }
       }
-
     })
+    
   },
   getUserInfo(e: any) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
@@ -82,5 +132,14 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  getUserAvatar(e:any) {
+    console.log(e)
+    this.setData({
+        canIUseOpenData:true,
+        
+    })
+    // 把获取到的微信头像的图像文件上传到后端
+
   }
 })
