3720a0d9f3aa2325682d8121c06e5280565247e7
VampireDetached <1478173643@qq.com>
2023-11-22 11:20:10 +0900

    activity-v3

--- frontend/miniprogram/pages/activity/activity.ts
+++ frontend/miniprogram/pages/activity/activity.ts
@@ -1,5 +1,5 @@
 // activity.ts
-
+import { areaList } from '../../miniprogram_npm/@vant/area-data/data';
 const app = getApp<IAppOption>()
 
 
@@ -12,6 +12,11 @@ Page({
     actcloud:false,
     activitylist:[],
     pageindex:0,
+    show: false,
+    fieldValue: '',
+    cascaderValue: '',
+    options:[],
+    areaList,
     navigationurl:"../../resource/navigationbar.png",
     adopturl:"../../resource/adopt.png",
     medicurl:"../../resource/medic.png",
@@ -71,20 +76,176 @@ Page({
       }
 
   },
+
+  actmedic(){
+    this.setData({
+        actmain:false,
+        actmedic:true,
+        pageindex:2
+    })
+    // var that = this
+    // wx.request({
+    //   url: 'http://43.143.139.4:8000/api/v1//',
+    //   data:{
+    //     openid:app.globalData.openid,        
+    //   },
+    //   method: 'GET',
+    //   header: {'content-type': 'application/json' //
+    //   },
+    //   success: function(res) {
+    //     that.setData({
+    //       activitylist: res.data
+    //     })
+    //     console.log(res.data)
+    //     console.log(that.data.actadopt)
+    //   },
+    //   fail:function(res){
+    //       console.log(res.errMsg)
+    //   }
+    // })
+  },
+
+  actlove(){
+    this.setData({
+        actmain:false,
+        actlove:true,
+        pageindex:3
+    })
+    var that = this
+    wx.request({
+      url: 'http://43.143.139.4:8000/api/v1/lovePet/',//todo
+      data:{
+        openid:app.globalData.openid,      
+      },
+      method: 'GET',
+      header: {'content-type': 'application/json' //
+      },
+      success: function(res) {
+         that.setData({
+           activitylist: res.data
+         })
+         console.log(res.data)
+         console.log(that.data.activitylist)
+      },
+      fail:function(res){
+           console.log(res.errMsg)
+      }
+    })
+  },
+
   editAdopt(){
     wx.navigateTo({
         url:"../editAdopt/editAdopt"
     })
   },
+  editLove(){
+    wx.navigateTo({
+        url:"../editLove/editLove"
+    })
+  },
   surf(e){
     console.log(e.currentTarget.id)
-    wx.switchTab({
-        url:"../petspace/petspace"
+    var aid = e.currentTarget.id
+    var pid = -1
+    for (var activ of this.data.activitylist)
+    {
+        if(activ.ActivityID==aid)
+        {
+            pid = activ.PetSpaceID
+            break
+        }
+    }
+    wx.navigateTo({
+        url:"../petdetail/petdetail?petspaceid="+pid
     })
   },
   adopt(e){
-    console.log(e.currentTarget.id)
+    console.log(e)
+
+    wx.request({
+        url: 'http://43.143.139.4:8000/api/v1/applyAdopt/',
+        data:{
+          openid:app.globalData.openid,
+          ActivityID:e.currentTarget.id
+        },
+        method: 'GET',
+        header: {'content-type': 'application/json' //
+        },
+        success: function(res) {
+            console.log(res.data.openid)
+        },
+        fail:function(res){
+            console.log(res.errMsg)
+        }
+      })
+  },
+  love(e){
+    var that = this
+    this.setData({
+      show: true,
+    });
+    wx.request({
+        url: 'http://43.143.139.4:8000/api/v1/applyLove/',
+        data:{
+          openid:app.globalData.openid
+        },
+        method: 'GET',
+        header: {'content-type': 'application/json' //
+        },
+        success: function(res) {
+          var op = []
+          for (let pet of res.data)
+          {
+              op.push({
+                  text:pet.name,
+                  value:pet.PetSpaceID
+              })
+          }
+          that.setData({
+              options:op,
+          })
+          
+          console.log(res.data)
+        },
+      })
+
+    console.log(e)
+
     
-  }
+  },
+  onClose() {
+    this.setData({
+      show: false,
+    });
+  },
+  onFinish(e) {
+    var that = this
+    const { selectedOptions, value } = e.detail;
+    const fieldValue = selectedOptions
+        .map((option) => option.text || option.name)
+        .join('/');
+    this.setData({
+      fieldValue,
+      cascaderValue: value,
+    })
+    wx.request({
+        url: 'http://43.143.139.4:8000/api/v1/applyLove/',//todo
+        data:{
+          openid:app.globalData.openid,
+          ActivityID:e.currentTarget.id,
+          petSpaceid:that.data.fieldValue,//todo
+          type:"love"
+        },
+        method: 'GET',
+        header: {'content-type': 'application/json' //
+        },
+        success: function(res) {
+            console.log(res.data.openid)
+        },
+        fail:function(res){
+            console.log(res.errMsg)
+        }
+      })
+  },
 
 });
