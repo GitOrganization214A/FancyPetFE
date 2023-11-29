b49e7863384f1ee4b578a5346ce222ef554a63b5
wangjiny21 <2945979072@qq.com>
2023-11-29 11:41:25 +0800

    2023.11.29-petspace-petspace show page

---
+++ frontend/miniprogram/pages/petdetailshow/petdetailshow.ts
@@ -0,0 +1,165 @@
+// pages/petdetailshow/petdetailshow.ts
+const app = getApp<IAppOption>()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    petspaceid:'',
+    number:0,
+    list:[
+      
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(event) {
+    console.log(event.petspaceid)
+    var that = this
+    this.data.petspaceid = event.petspaceid
+    wx.request({
+      url: 'http://43.143.139.4:8000/api/v1/viewPetSpace/',
+      method:"GET",
+      header: {'content-type': 'application/json' //
+      },
+      data:{
+        PetSpaceID:this.data.petspaceid
+      },
+      success:function(res) {
+          that.setData({
+            PetSpaceDetail: res.data
+          })
+          console.log(res.data.images)
+          for (let image of res.data.images)
+            {
+              that.data.number=that.data.number+1
+              var numstring=that.data.number.toString()
+              var numberlist=[{
+                newUrl:image,
+                key:numstring
+              }]
+              that.data.list=[...that.data.list,...numberlist]
+            }
+            console.log(that.data.list)
+      }
+    })
+  },
+  deletePetSpace:function(event){
+    console.log(event.petspaceid)
+    wx.request({
+      url: 'http://43.143.139.4:8000/api/v1/deletePetSpace/',
+      method:"GET",
+      header: {'content-type': 'application/json' //
+      },
+      data:{
+        PetSpaceID:this.data.petspaceid
+      },
+      success:function(res) {
+        wx.switchTab({
+          url:'/pages/petspace/petspace'
+        })
+      }
+    })
+  },
+  addremind:function(e){
+
+  },
+  addrecord:function(e){
+
+  },
+  takephoto:function(e){
+    wx.navigateTo({
+      url:'/pages/photo/photo'
+    })
+  },
+  deletePhoto:function(e){
+    var index = this.data.number;
+    console.log(e)
+    var petspaceid=this.data.petspaceid
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除此图片吗？',
+      success: function (res) {
+       if (res.confirm) {
+        console.log('点击确定了');
+        wx.request({
+          url: 'http://43.143.139.4:8000/api/v1/deletePhoto/',
+          method:"GET",
+          header: {'content-type': 'application/json' //
+          },
+          data:{
+            PetSpaceID:petspaceid,
+            index:index
+          },
+          success:function(res) {
+            wx.navigateTo({
+              url:'/pages/petdetail/petdetail?petspaceid='+app.globalData.petspaceid,
+            })
+          }
+        })
+       } else if (res.cancel) {
+         console.log('点击取消了');
+         return false;   
+        }
+      }
+   
+     })
+  },
+  getimageID:function(e){
+    console.log(e)
+    this.setData({
+      number:e.detail.index
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
