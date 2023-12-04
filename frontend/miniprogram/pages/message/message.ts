901d800bcf1d4604ad03108ce6bfb513b45064c2
VampireDetached <1478173643@qq.com>
2023-12-04 20:55:33 +0900

    2023.12.2-message-message type

--- frontend/miniprogram/pages/message/message.ts
+++ frontend/miniprogram/pages/message/message.ts
@@ -6,7 +6,8 @@ const app = getApp<IAppOption>()
 Page({
   data: {
     messageList:[],
-    navigationurl:"../../resource/navigationbar.png"
+    navigationurl:"../../resource/navigationbar.png",
+    deleteurl:"../../resource/giveup.png"
   },
   onShow(){
     var that = this
@@ -30,130 +31,38 @@ Page({
         }
       })
   },
-  
-  actparty(){
-    this.setData({
-        actmain:false,
-        actparty:true,
-        pageindex:2
+  surfindex(e){
+    const tempopenid = e.target.dataset.index
+    wx.navigateTo({
+      url:'/pages/userinfo/userinfo?openid='+tempopenid,
     })
-    var that = this
-    
-      
   },
-
-  actlove(){
-    this.setData({
-        actmain:false,
-        actlove:true,
-        pageindex:3
-    })
+  deletemessage(e){
     var that = this
+    const mid = e.target.dataset.index
     wx.request({
-      url: 'http://43.143.139.4:8000/api/v1/lovePet/',//todo
-      data:{
-        openid:app.globalData.openid,      
-      },
-      method: 'GET',
-      header: {'content-type': 'application/json' //
-      },
-      success: function(res) {
-         that.setData({
-           activitylist: res.data
-         })
-         that.data.activitylist = res.data
-         console.log(res.data)
-         console.log(that.data.activitylist)
-      },
-      fail:function(res){
-           console.log(res.errMsg)
-      }
-    })
-  },
-
-  editAdopt(){
-    wx.navigateTo({
-        url:"../editAdopt/editAdopt"
-    })
-  },
-  editLove(){
-    wx.navigateTo({
-        url:"../editLove/editLove"
-    })
-  },
-  editParty(){
-    wx.navigateTo({
-        url:"../editParty/editParty"
-    })
-  },
-  surf(e){
-    console.log(e)
-    var aid = e.currentTarget.dataset.index
-    var pid = -1
-    for (var activ of this.data.activitylist)
-    {
-        if(activ.ActivityID==aid)
-        {
-            pid = activ.PetSpaceID
-            break
-        }
-    }
-    console.log(aid)
-    console.log(pid)
-    wx.navigateTo({
-        url:"../petdetail/petdetail?petspaceid="+pid
-    })
-  },
-  surfParty(e){
-    var aid = e.currentTarget.dataset.index
-    for(let party of this.data.activitylist)
-    {
-        if(party.ActivityID==aid)
-        {
-            this.setData({
-                showparty:true,
-                partyUrl:party.img,
-                partytitle:party.title,
-                partydate:party.time,
-                partyaddress:party.address,
-                partycontent:party.content
-            })
-        }
-    }
-  },
-  adopt(e){
-    console.log(e)
-    this.setData({
-        showadopt:true,
-        adoptTarget:e.currentTarget.id
-    })
-  },
-  deleteactivity(e){
-    var that = this
-    wx.request({
-        url: 'http://43.143.139.4:8000/api/v1/deleteActivity/',//todo
+        url: 'http://43.143.139.4:8000/api/v1/deleteMessage/',
         data:{
-          openid:app.globalData.openid,  
-          ActivityID:e.currentTarget.dataset.index
+          openid:app.globalData.openid,
+          MessageID:mid
         },
         method: 'GET',
         header: {'content-type': 'application/json' //
         },
         success: function(res) {
-            var aid = e.currentTarget.dataset.index
-            var aindex = -1
-            var templist = that.data.activitylist
-            for (var activ of templist)
+            var mindex = -1
+            var templist = that.data.messageList
+            for (var msg of templist)
             {
-                aindex ++
-                if(activ.ActivityID==aid)
+                mindex++
+                if(msg.MessageID==mid)
                 {
-                    templist.splice(aindex,1)
+                    templist.splice(mindex,1)
                     that.setData({
-                        activitylist:templist
+                        messageList:templist
                     })
-                    that.data.activitylist=templist
-                    console.log(that.data.activitylist)
+                    that.data.messageList=templist
+                    console.log(that.data.messageList)
                     break
                 }
             }
@@ -162,126 +71,5 @@ Page({
             console.log(res.errMsg)
         }
       })
-  },
-  inputadopt:function(e){
-    var value = e.detail.value;
-    var len = parseInt(value.length)
-    this.setData({
-        currentadoptNumber: len,
-        adoptcontent: value
-    })
-    this.data.currentadoptNumber=len
-    this.data.adoptcontent=value    
-    console.log(this.data.adoptcontent)
-  },
-  guadopt(e){
-    this.setData({
-        showadopt:false,
-        currentadoptNumber: 0,
-        adoptcontent: ''
-    })
-  },
-  guparty(e){
-    this.setData({
-        showparty:false,
-    })
-  },
-  sendadopt(e){
-    var that = this 
-    wx.request({
-        url: 'http://43.143.139.4:8000/api/v1/applyAdopt/',
-        data:{
-          openid:app.globalData.openid,
-          ActivityID:that.data.adoptTarget,
-          content:that.data.adoptcontent
-        },
-        method: 'GET',
-        header: {'content-type': 'application/json' //
-        },
-        success: function(res) {
-            console.log(res.data.openid)
-            wx.showToast({
-                title: '发送成功！',
-                icon: 'none',
-                duration: 2000
-            })
-            that.setData({
-                showadopt:false,
-                currentadoptNumber: 0,
-                adoptcontent: ''
-            })
-            that.data.currentadoptNumber=0
-            that.data.adoptcontent='' 
-        },
-        fail:function(res){
-            console.log(res.errMsg)
-        }
-    })
-  },
-  love(e){
-    wx.navigateTo({
-        url:"../editsendLove/editsendLove?activityid="+e.currentTarget.dataset.index
-    })
-  },
-  onClose() {
-    this.setData({
-      show: false,
-    });
-  },
-  onFinish(e) {
-    var that = this
-    console.log(e)
-    const { selectedOptions, value } = e.detail;
-    const fieldValue = selectedOptions
-        .map((option) => option.text || option.name)
-        .join('/');
-    this.setData({
-      fieldValue,
-      cascaderValue: value,
-    })
-    wx.request({
-        url: 'http://43.143.139.4:8000/api/v1/applyLove/',//todo
-        data:{
-          openid:app.globalData.openid,
-          ActivityID:e.currentTarget.id,
-          petSpaceid:that.data.fieldValue,//todo
-          type:"love"
-        },
-        method: 'GET',
-        header: {'content-type': 'application/json' //
-        },
-        success: function(res) {
-            console.log(e.currentTarget.id)
-            console.log(that.data.fieldValue)
-            console.log(res.data.openid)
-        },
-        fail:function(res){
-            console.log(res.errMsg)
-        }
-      })
-  },
-  participate(e){
-    var that = this 
-    wx.request({
-        url: 'http://43.143.139.4:8000/api/v1/applyParty/',
-        data:{
-          openid:app.globalData.openid,
-          ActivityID:e.currentTarget.dataset.index
-        },
-        method: 'GET',
-        header: {'content-type': 'application/json' //
-        },
-        success: function(res) {
-            console.log(res.data.openid)
-            wx.showToast({
-                title: '发送成功！',
-                icon: 'none',
-                duration: 2000
-            })
-        },
-        fail:function(res){
-            console.log(res.errMsg)
-        }
-    })
   }
 });
